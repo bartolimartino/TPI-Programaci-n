45d7bf8e032406ca46c90cb28bce4172b3b770c7
bartolimartino <bartolimartino01@gmail.com>
2026-06-15 13:43:45 -0300

    Cambio en las funciones - opcion 3
    Se realizaron cambios dentro de la función correspondiente a la opcion - buscar por nombre

--- Buscar_por_nombre.py
+++ Buscar_por_nombre.py
@@ -11,29 +11,26 @@ def eliminar_tildes(texto):
     )
     return texto
 
-def buscar_pais(archivo):
+def buscar_pais(lista_paises):
     print("\n---| Buscar un País por Nombre |---")
     busqueda = eliminar_tildes(input("Ingrese el nombre del país a buscar: "))
 
     encontrado = False
     try:
-        with open(archivo, mode="r", encoding="utf-8") as archivo:
-            lector = csv.DictReader(archivo)
-
-            for fila in lector:
-                if eliminar_tildes(fila["nombre"]) == busqueda:
-                    print("\n¡País Encontrado!")
-                    print(f" Nombre:     {fila['nombre']}")
-                    print(f" Población:  {fila['poblacion']}")
-                    print(f" Superficie: {fila['superficie']} km²")
-                    print(f" Continente: {fila['continente']}")
-                    encontrado = True
-                    break  
+        for fila in lista_paises:
+            if eliminar_tildes(fila["nombre"]) == busqueda:
+                print("\n¡País Encontrado!")
+                print(f" Nombre:     {fila['nombre']}")
+                print(f" Población:  {fila['poblacion']}")
+                print(f" Superficie: {fila['superficie']} km²")
+                print(f" Continente: {fila['continente']}")
+                encontrado = True
+                break  
 
         if not encontrado:
             print(f"\nEl país '{busqueda.title()}' no está en la enciclopedia.")
 
     except FileNotFoundError:
-        print(f"\n Error: El archivo '{archivo}' aún no existe. Cargá un país primero.")
+        print(f"\n Error: El archivo aún no existe. Cargá un país primero.")
 
 
